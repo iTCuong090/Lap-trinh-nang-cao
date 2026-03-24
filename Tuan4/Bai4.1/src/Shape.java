eb6a82868f1ec2423a49e38a7ff062ba83a2a91f
Tạ Hữu Cường <dragonsvip090@gmail.com>
2026-03-24 09:04:04 +0700

    xong bai 4.1

---
+++ Tuan4/Bai4.1/src/Shape.java
@@ -0,0 +1,48 @@
+public abstract class Shape {
+    protected int x, y;
+
+    public Shape(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public abstract void erase();
+    public abstract void draw();
+
+    public void moveTo(int newX, int newY) {
+        erase();
+        x = newX;
+        y = newY;
+        draw();
+    }
+}
+
+class Circle extends Shape {
+    @Override
+    public void erase() {
+        System.out.println("Xóa hình tròn tại ("+x+","+y+")");
+    }
+    @Override
+    public void draw() {
+        System.out.println("Vẽ hình tròn tại ("+x+","+y+")");
+    }
+
+    public Circle(int x, int y) {
+        super(x, y);
+    }
+}
+
+class Square extends Shape {
+    @Override
+    public void erase() {
+        System.out.println("Xóa hình vuông tại ("+x+","+y+")");
+    }
+    @Override
+    public void draw() {
+        System.out.println("Vẽ hình vuông tại ("+x+","+y+")");
+    }
+
+    public Square(int x, int y) {
+        super(x, y);
+    }
+}
